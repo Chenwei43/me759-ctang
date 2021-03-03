258da8075189a9fa714cdda25e9ee93967554487
Chenwei43 <chenwei.tang@gmail.com>
2021-03-02 19:53:16 -0600

    padded stencil

--- HW04/stencil.cu
+++ HW04/stencil.cu
@@ -10,59 +10,82 @@
 #include "stencil.cuh"
 
 __global__ void stencil_kernel(const float* image, const float* mask, float* output, unsigned int n, unsigned int R) {
-
+	// Computes the convolution of image and mask, storing the result in output.
+	// Each thread should compute _one_ element of output.
+	// Shared memory should be allocated _dynamically_ only.
+	//
+	// image is an array of length n of managed memory.
+	// mask is an array of length (2 * R + 1) of managed memory.
+	// output is an array of length n of managed memory.
+	//
+	// Assumptions:
+	// - 1D configuration
+	// - blockDim.x >= 2 * R + 1
+	//
 	int maskSize = 2 * R + 1;
-	int imageIdxL = blockIdx.x * blockDim.x;
-    int imageIdxR =(blockIdx.x+1)  * blockDim.x;
 	int index = threadIdx.x + blockIdx.x * blockDim.x;
-	int outputIndex = blockDim.x + maskSize + threadIdx.x;
-    if (index < n){
+	int index_padded = threadIdx.x + R;
+	int outputIndex = blockDim.x + 2*R + maskSize + threadIdx.x;
 
-        // sharedArray contains
-        // - The entire mask
-        // - The elements of image needed to compute the elements of output corresponding to the threads in the given block
-        // - The output image elements corresponding to the given block before it is written back to global memory
-        extern __shared__ float sharedArray[];
-        
-        // load tiles from global mem to shared mem
-        sharedArray[threadIdx.x] = image[index];
+	if (index < n) {
+		// sharedArray contains
+		// - The entire mask
+		// - The elements of image needed: KEY!! it's 2R + num_threads
+		// - The output image elements corresponding to the given block before it is written back to global memory
+		extern __shared__ float sharedArray[];
 
-        if (threadIdx.x < maskSize) {
-            sharedArray[blockDim.x + threadIdx.x] = mask[threadIdx.x];
-        }
+		// load tiles from global mem to shared mem
+		sharedArray[index_padded] = image[index];
+		if (threadIdx.x < R) {
+			if (int(index - R) < 0) {
+				sharedArray[index_padded - R] = 1;
+			}
+			else
+			{
+				sharedArray[index_padded - R] = image[index - R];
+			}
+			if (index + blockDim.x > n - 1) {
+				sharedArray[index_padded + blockDim.x] = 1;
+			}
+			else {
+				sharedArray[index_padded + blockDim.x] = image[index + blockDim.x];
+			}
 
-        sharedArray[outputIndex] = 0.0;					
-        __syncthreads();
+		}
 
-        // conv
-        //float outputSub = 0.0;
-        for (int k = 0; k < maskSize; k++) {
-            int imageIndex = index - R + k;   
-            if (imageIndex >= imageIdxL && imageIndex < imageIdxR) {
-                sharedArray[outputIndex] += sharedArray[threadIdx.x + k - R] * sharedArray[blockDim.x + k];
-                //outputSub += sharedArray[threadIdx.x + k - R] * sharedArray[blockDim.x + k];
-            }
-            else
-            {
-                sharedArray[outputIndex] += sharedArray[blockDim.x + k];
-                //outputSub += sharedArray[blockDim.x + k];
-            }              
-            
-        }
-        __syncthreads();            
-                
-        // move output to global mem
-        output[index] = sharedArray[outputIndex];
-        // output[index] = outputSub;
-    }
-	
+		if (threadIdx.x < maskSize) {
+			sharedArray[blockDim.x + 2 * R + threadIdx.x] = mask[threadIdx.x];
+		}
+
+		sharedArray[outputIndex] = 0.0;
+		__syncthreads();
+
+		// conv		
+		for (int k = -R; k < int(R+1); k++) {
+			sharedArray[outputIndex] += sharedArray[index_padded + k] * sharedArray[blockDim.x + 3 * R + k];
+		}
+		__syncthreads();
+
+
+		// move output to global mem
+		output[index] = sharedArray[outputIndex];
+	}
 }
 
-
 __host__ void stencil(const float* image, const float* mask, float* output, unsigned int n, unsigned int R, unsigned int threads_per_block) {
-
+	// Computes the convolution of image and mask, storing the result in output.
+	// image is an array of length n of managed memory.
+	// mask is an array of length (2 * R + 1) of managed memory.
+	// output is an array of length n of managed memory.
+	// Makes one call to stencil_kernel with threads_per_block threads per block.
+	// The kernel call should be followed by a call to cudaDeviceSynchronize for timing purposes.
+	//
+	// Assumptions:
+	// - threads_per_block >= 2 * R + 1
 	int maskSize = 2 * R + 1;
 	unsigned int numBlocks = (n + threads_per_block - 1) / threads_per_block;
-	stencil_kernel<<<numBlocks, threads_per_block, (threads_per_block * 2 + maskSize) * sizeof(float)>>>(image, mask, output, n, R);
+
+	// Shared mem: R-threads-R of image (padding of Radius needed at start and end), mask and output
+	stencil_kernel<<<numBlocks, threads_per_block, (threads_per_block*2 + 2*R +maskSize) * sizeof(float)>>>(image, mask, output, n, R);
 	cudaDeviceSynchronize();
 }
