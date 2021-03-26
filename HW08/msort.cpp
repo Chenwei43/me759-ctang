90fa4bf2120cebb8a2d395861b420c0fda3a1244
Chenwei43 <chenwei.tang@gmail.com>
2021-03-25 20:21:25 -0500

    task3 partially done

--- HW08/msort.cpp
+++ HW08/msort.cpp
@@ -13,7 +13,57 @@
 // the threshold, a serial sort algorithm will be used to avoid overhead
 // of task scheduling
 void merge(int *arr, unsigned int idL, unsigned int idR, unsigned int mid){
-    
+
+    unsigned int len1 = mid-idL+1;
+    unsigned int len2 = idR-mid;
+    int temp1[len1], temp2[len2];
+
+    for (unsigned int i=0; i<len1; i++){
+        temp1[i] = arr[idL+i];
+    }
+    for (unsigned int j=0; j<len2; j++){
+        temp2[j] = arr[mid + 1+j];
+    }
+
+    // start comparing (ascending)
+    unsigned int i=0;
+    unsigned int j=0;
+    unsigned int k=idL;
+    while(i<len1 && j<len2){
+        if(temp1[i]<=temp2[j]){
+            arr[k] = temp1[i];
+            i++;
+        }else{
+            arr[k] = temp2[j];
+            j++;
+        }
+        k++;
+    }
+    if(i<len1){
+        for(unsigned int res=i; res<len1; res++){
+            arr[k]=temp1[res];
+            k++;
+        }
+    }else if(j<len2){
+        for(unsigned int res=j; res<len2; res++){
+            arr[k]=temp2[res];
+            k++;
+        }
+    }
+    // while (i < len1) {
+    //     arr[k] = temp1[i];
+    //     i++;
+    //     k++;
+    // }
+ 
+    // // Copy the remaining elements of
+    // // R[], if there are any
+    // while (j < len2) {
+    //     arr[k] = temp2[j];
+    //     j++;
+    //     k++;
+    // }
+
 }
 
 
@@ -21,13 +71,24 @@ void splitSort(int* arr, unsigned int idL, unsigned int idR){
     if (idL>=idR){
         return;
     }
-    unsigned int mid = idL+(idR-1)/2;
+    unsigned int mid = idL+(idR-idL)/2;
     splitSort(arr, idL, mid);
     splitSort(arr, mid+1, idR);
     merge(arr, idL, idR, mid);
 
 }
 
-void msort(int* arr, const std::size_t n, const std::size_t threshold){
 
+void msort(int* arr, const std::size_t n, const std::size_t threshold){
+    if (n<threshold){
+        splitSort(arr, 0, n-1);
+    }else{
+        // parallel recursive
+#pragma omp parallel
+    {
+        splitSort(arr, 0, n-1);
+    }
+
+
+    }
 }
--- HW08/task3.cpp
+++ HW08/task3.cpp
@@ -21,22 +21,21 @@ int main(int argc, char* argv[]){
         int* arr = new int[n];
         int my_seed = 137;
         std::mt19937 generator(my_seed);
-        std::uniform_real_distribution<int> dist1(-1000, 1000);
+        std::uniform_int_distribution<int> dist1(-1000, 1000);
         for (size_t i = 0; i < n; i++) {            
             arr[i] = dist1(generator);	        					
         }
-
         high_resolution_clock::time_point start;
         high_resolution_clock::time_point end;
         duration<double, std::milli> duration_sec;
         start = high_resolution_clock::now();
-#pragma omp parallel num_threads(t)
+        omp_set_num_threads(t);
         msort(arr, n, ts);
         end = high_resolution_clock::now();
         duration_sec = std::chrono::duration_cast<duration<double, std::milli>>(end - start);
 
         cout << arr[0] << "\n";
-        cout << arr[n*n - 1] << "\n";
+        cout << arr[n - 1] << "\n";
         cout << duration_sec.count() << "\n";
 
         timefile << t << " " << duration_sec.count() << "\n";
