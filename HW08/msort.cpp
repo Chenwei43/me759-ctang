c38a0ee37385b9ecfa859436e596c0f0df921ed7
Chenwei43 <chenwei.tang@gmail.com>
2021-03-25 20:51:23 -0500

    task3 done

--- HW08/msort.cpp
+++ HW08/msort.cpp
@@ -1,4 +1,6 @@
 #include <omp.h>
+#include <stdio.h>
+#include <cstring>
 #include "msort.h"
 
 
@@ -12,83 +14,57 @@
 // start making parallel recursive calls. If the size of array goes below
 // the threshold, a serial sort algorithm will be used to avoid overhead
 // of task scheduling
-void merge(int *arr, unsigned int idL, unsigned int idR, unsigned int mid){
+void merge(int *arr, unsigned int n){
 
-    unsigned int len1 = mid-idL+1;
-    unsigned int len2 = idR-mid;
-    int temp1[len1], temp2[len2];
-
-    for (unsigned int i=0; i<len1; i++){
-        temp1[i] = arr[idL+i];
-    }
-    for (unsigned int j=0; j<len2; j++){
-        temp2[j] = arr[mid + 1+j];
-    }
+    int *temp = new int[n];
 
     // start comparing (ascending)
     unsigned int i=0;
-    unsigned int j=0;
-    unsigned int k=idL;
-    while(i<len1 && j<len2){
-        if(temp1[i]<=temp2[j]){
-            arr[k] = temp1[i];
+    unsigned int j=n/2;
+    unsigned int k=0;
+    while(i<n/2 && j<n){
+        if(arr[i]<=arr[j]){
+            temp[k] = arr[i];
             i++;
         }else{
-            arr[k] = temp2[j];
+            temp[k] = arr[j];
             j++;
         }
         k++;
     }
-    if(i<len1){
-        for(unsigned int res=i; res<len1; res++){
-            arr[k]=temp1[res];
+    if(i<n/2){
+        for(unsigned int res=i; res<n/2; res++){
+            temp[k]=arr[res];
             k++;
         }
-    }else if(j<len2){
-        for(unsigned int res=j; res<len2; res++){
-            arr[k]=temp2[res];
+    }else if(j<n){
+        for(unsigned int res=j; res<n; res++){
+            temp[k]=arr[res];
             k++;
         }
     }
-    // while (i < len1) {
-    //     arr[k] = temp1[i];
-    //     i++;
-    //     k++;
-    // }
- 
-    // // Copy the remaining elements of
-    // // R[], if there are any
-    // while (j < len2) {
-    //     arr[k] = temp2[j];
-    //     j++;
-    //     k++;
-    // }
-
-}
-
-
-void splitSort(int* arr, unsigned int idL, unsigned int idR){
-    if (idL>=idR){
-        return;
-    }
-    unsigned int mid = idL+(idR-idL)/2;
-    splitSort(arr, idL, mid);
-    splitSort(arr, mid+1, idR);
-    merge(arr, idL, idR, mid);
-
+    memcpy(arr, temp, n*sizeof(int));
+    delete []temp;
 }
 
 
 void msort(int* arr, const std::size_t n, const std::size_t threshold){
-    if (n<threshold){
-        splitSort(arr, 0, n-1);
-    }else{
-        // parallel recursive
-#pragma omp parallel
+
+    if (n<=1) return;
+    
+    #pragma omp task shared(arr) if (n>threshold)
+    {   
+        msort(arr, n/2, threshold);
+    }
+    #pragma omp task shared(arr) if (n>threshold)
     {
-        splitSort(arr, 0, n-1);
+        msort(arr+n/2, n/2, threshold);
     }
-
-
+    #pragma omp taskwait
+    {
+        merge(arr, n);
     }
+        
+ 
+
 }
--- HW08/task3.cpp
+++ HW08/task3.cpp
@@ -4,6 +4,8 @@
 #include <random>
 #include <chrono>
 #include <sstream>
+#include <stdio.h>
+#include <cstring>
 
 #include "msort.h"
 using namespace std;
@@ -12,34 +14,40 @@ using chrono::duration;
 
 int main(int argc, char* argv[]){
     unsigned int n = std::strtoul(argv[1], nullptr, 10);
-    unsigned int ts = std::strtoul(argv[2], nullptr, 10);
+    unsigned int t = std::strtoul(argv[2], nullptr, 10);
+    unsigned int ts = std::strtoul(argv[3], nullptr, 10);
 
-    std::ofstream timefile;
-    timefile.open("timingTask3.txt");
+    // std::ofstream timefile;
+    // timefile.open("timingTask3_t.txt");
 
-    for (unsigned int t=8; t<9; t++){
-        int* arr = new int[n];
-        int my_seed = 137;
-        std::mt19937 generator(my_seed);
-        std::uniform_int_distribution<int> dist1(-1000, 1000);
-        for (size_t i = 0; i < n; i++) {            
-            arr[i] = dist1(generator);	        					
-        }
-        high_resolution_clock::time_point start;
-        high_resolution_clock::time_point end;
-        duration<double, std::milli> duration_sec;
-        start = high_resolution_clock::now();
-        omp_set_num_threads(t);
-        msort(arr, n, ts);
-        end = high_resolution_clock::now();
-        duration_sec = std::chrono::duration_cast<duration<double, std::milli>>(end - start);
+    // for (unsigned int t=1; t<21; t++){
 
-        cout << arr[0] << "\n";
-        cout << arr[n - 1] << "\n";
-        cout << duration_sec.count() << "\n";
-
-        timefile << t << " " << duration_sec.count() << "\n";
+    int* arr = new int[n];
+    int my_seed = 137;
+    std::mt19937 generator(my_seed);
+    std::uniform_int_distribution<int> dist1(-1000, 1000);
+    for (size_t i = 0; i < n; i++) {            
+        arr[i] = dist1(generator);	        					
     }
+    high_resolution_clock::time_point start;
+    high_resolution_clock::time_point end;
+    duration<double, std::milli> duration_sec;
+    start = high_resolution_clock::now();
+    omp_set_num_threads(t);
+#pragma omp parallel
+    {
+        #pragma omp single
+        msort(arr, n, ts);
+    }  
+    end = high_resolution_clock::now();
+    duration_sec = std::chrono::duration_cast<duration<double, std::milli>>(end - start);
+
+    cout << arr[0] << "\n";
+    cout << arr[n - 1] << "\n";
+    cout << duration_sec.count() << "\n";
+
+    //     timefile << t << " " << duration_sec.count() << "\n";
+    // }
 
     return 0;
 
